436450d2ee29a69a0b1e8865146fbc6e09177050
Noisey Turtle <bobbyoldani2@gmail.com>
2020-01-18 13:02:53 -0800

    created variables for doc info

--- script.js
+++ script.js
@@ -21,6 +21,19 @@ $.ajax({
     .then(function(response) {
       console.log(response);
 
+      let docName = response.data[0].profile["first_name"];
+      console.log("Name: " + docName);
+      let docSpec = response.data[0].specialties[0].uid;
+      console.log("Specialty: " + docSpec);
+      let docClinic = response.data[0].practices[0].name;
+      console.log("Clinic: " + docClinic);
+      let docLat = response.data[0].practices[0].lat
+      console.log("Lat: " + docLat)
+      let docLon = response.data[0].practices[0].lon
+      console.log("lon :" + docLon);
+      let docNum = response.data[0].practices[0].phones[0].number
+      console.log("Number: " + docNum);
+      
 
 
     })
